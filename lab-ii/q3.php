388031aa3faf8d50aca99f1e1ba4b47c99ce271e
srynprjl <mail@shreyanparajuli.com.np>
2024-11-21 08:50:00 +0545

    Update q3.php

--- lab-ii/q3.php
+++ lab-ii/q3.php
@@ -1,4 +1,18 @@
 <?php
-    
+    // reverse string
+
+    function reverseStr($str){
+        $len = strlen($str) - 1;
+        $newStr = "";
+        $i = $len;
+        while($i >= 0){
+            $val = $str[$i];
+            echo $val;
+            $i--;       
+        }
+    }
+
+    echo reverseStr("Shreyan");
+
 
 ?>
